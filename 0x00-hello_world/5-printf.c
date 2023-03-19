24f138c531c74937ad63a21bb2bb9afcb3ffed20
aliola123 <alipoemoses646@gmail.com>
2023-03-19 07:05:48 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * main_description: prints the phrase "with proper grammar, but the outcome...
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
