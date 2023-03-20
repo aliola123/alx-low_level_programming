a4d17ec990d88643677b66ec1b5ebceeb36e03e9
aliola123 <alipoemoses646@gmail.com>
2023-03-20 09:01:11 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description_main -
+ * Return: 0 if run successfully, non zero value otherwise
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
