ec02a859416734f4ce0a531ca9eff4fc721d77a2
aliola123 <alipoemoses646@gmail.com>
2023-03-22 10:54:17 -0700

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
 void print_alphabet_x10(void)
 {
 	char i;
-	int j = 0 ;
+	int j = 0;
 
 	while (j <= 9)
 	{
@@ -16,6 +16,7 @@ void print_alphabet_x10(void)
 		_putchar(i);
 	}
 	_putchar('\n');
+	j++;
 	}
 }
 
