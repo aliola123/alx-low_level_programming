a4449567dba25e7d7980e70c94ea5cc4d8a698e0
aliola123 <alipoemoses646@gmail.com>
2023-03-22 13:40:02 -0700

    l

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,19 @@
 #include "main.h"
 
+/**
+ * _islower - checks for lowercase character
+ * @c: - the value to be checked
+ * Return: 1 if character lowercase, 0 otherwise
+ */
 
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: the value to be checked
+ * Return: 1 if checked successfully, 0 value otherwise
+ */
+
+int _isalpha(int c)
+{
+	
