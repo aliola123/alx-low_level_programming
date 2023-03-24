977ac1484f7d5d21c577f627582edc300174d5be
aliola123 <alipoemoses646@gmail.com>
2023-03-24 09:41:01 -0700

    10 times trouble

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * followed by a new line
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+
+	while (j < 10)
+	{
+	for (i = 0 ; i <= 14 ; i++)
+	{
+		if (i > 9)
+			_putchar(i / 10 + '0');
+		_putchar (i % 10 + '0');
+	}
+	_putchar('\n');
+	 j++;
+	}
+}
