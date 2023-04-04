b53bda3758861130c2a815631f2877312f0d9147
aliola123 <alipoemoses6461@gmail.com>
2023-04-04 03:54:49 +0100

    pulling to ssh

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: - adress of memory to be filled
+ * @b: the constant byte to be filled in
+ * @n: number of bytes required
+ * Return: changed array with a new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *s;
+
