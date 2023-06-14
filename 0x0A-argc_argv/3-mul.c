00213951dfa7312ff5c04c6d352ca027e92d03d9
aliola123 <alipoemoses6461@gmail.com>
2023-06-14 10:13:29 +0100

    betty

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * main - entry point for an argument conditional multiplication program
+ * @argc: counts number of arguments passed to program
+ * @argv: pointer to an array of strings
+ *
+ * mul - A function that multiplies two numbers
+ * @a: variable to be multiplied
+ * @b: variable to be multiplied
+ *
+ * Return: 0 if argc is not less than 2 and 1 other wise
+ */
 
 int mul(int a, int b)
 {
@@ -8,13 +20,13 @@ int mul(int a, int b)
 	mul = a * b;
 
 	printf("%d\n", mul);
-	
 	return (0);
 }
 
 int main(int argc, char *argv[])
 {
 	int a, b;
+
 	if (argc < 2)
 	{
 		printf("Error\n");
