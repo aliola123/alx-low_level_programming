a344003d32cdc2e69f4bde68b5bc7034c2ed0ed1
aliola123 <alipoemoses6461@gmail.com>
2023-06-23 01:52:21 +0100

    testing

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,18 +1,28 @@
+/**
+ * sum_them_all - a function that sums all argumentsi
+ * Return: sum to the main function
+ * @n: the counter for loop
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-    int sum = 0;
-    int i;
-    if(n == 0)
-    {
-        return 0;
-    }
-    va_list my_number;
-    va_start(my_number, n);
-    for(i = 0; i < n; i++)
-    {
-        int c = va_arg(my_number, int);
-        sum = sum + c;
-    }
-    va_end(my_number);
-    return(sum);
+	int sum = 0;
+	int i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_list my_number;
+
+	va_start(my_number, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int c = va_arg(my_number, int);
+	sum = sum + c;
+	}
+	va_end(my_number);
+
+	return (sum);
 }
